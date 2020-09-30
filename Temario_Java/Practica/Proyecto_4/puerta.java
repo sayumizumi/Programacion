4293f876cefaf5a64bea0c32b72fcd8ea54c0eb9
Marta <marta0635@gmail.com>
2020-09-30 20:34:13 +0200

    [30.09.2020 - 20:33]
    primer arreglo

--- Temario_Java/Practica/Proyecto_4/puerta.java
+++ Temario_Java/Practica/Proyecto_4/puerta.java
@@ -1,37 +1,37 @@
-
-// Necesitamos hacer una puerta con contraseña, al iniciar el programa se nos pedirá introducir una contraseña.
-// una vez introducida, se nos mostraran dos opciones (1. cambiar contraseña, 2. cerrar puerta). En la primera opción
-// se nos pedira la contraseña actual y luego la nueva. En la segunda opción se cerrara el programa
-import java.util.*;
-
-/**
- * puerta
- */
-public class puerta {
-
-    public static void main(String[] args) {
-
-        // variables
-        String contraseña, nuevacontraseña;
-        Scanner sc = new Scanner(System.in);
-        int seleccion;
-
-        System.out.println("Dime la contra");
-        contraseña = sc.next();
-        if (contraseña == "soyadmin") {
-            System.out.println("eeeee wachon q quieres hacer");
-            System.out.println("1. Cambiar contraseña");
-            System.out.println("2. Cerrar esta movida");
-            seleccion = sc.nextInt();
-            if (seleccion == 1) { System.out.println("pos dime una nueva");
-            sc.next();
-                
-            }
-
-        } else {
-            System.out.println("te mamaste bro esa no es");
-
-        }
-
-    }
+ 
+// Necesitamos hacer una puerta con contraseña, al iniciar el programa se nos pedirá introducir una contraseña. 
+// una vez introducida, se nos mostraran dos opciones (1. cambiar contraseña, 2. cerrar puerta). En la primera opción 
+// se nos pedira la contraseña actual y luego la nueva. En la segunda opción se cerrara el programa 
+import java.util.*; 
+ 
+/** 
+ * puerta 
+ */ 
+public class puerta { 
+ 
+    public static void main(String[] args) { 
+ 
+        // variables 
+        String contraseña, nuevacontraseña; 
+        Scanner sc = new Scanner(System.in); 
+        int seleccion; 
+ 
+        System.out.println("Dime la contra"); 
+        contraseña = sc.next(); 
+        if(contraseña.equals("soyadmin")) {
+            System.out.println("eeeee wachon q quieres hacer"); 
+            System.out.println("1. Cambiar contraseña"); 
+            System.out.println("2. Cerrar esta movida"); 
+            seleccion = sc.nextInt(); 
+            if (seleccion == 1) { System.out.println("pos dime una nueva"); 
+            
+                 
+            } 
+ 
+        } else { 
+            System.out.println("te mamaste bro esa no es"); 
+ 
+        } 
+ 
+    } 
 }
